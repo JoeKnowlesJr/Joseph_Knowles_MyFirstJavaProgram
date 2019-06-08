61e7735ce33ccfa22e31bdeb092ede0f901ccdef
Joe Knowles <50335840+JoeKnowlesJr@users.noreply.github.com>
2019-06-08 15:29:17 -0500

    Add files via upload

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class MyFirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Joseph Knowles");
+        System.out.println("I am 48 years old");
+        System.out.println("My hometown is Baltimore, MD");
+    }
+}
